e38e5c90fbb7fa12104ea2ebbf71bc4e7cbd346e
Akash Kumar <37481156+akashkumar2k10@users.noreply.github.com>
2020-08-02 10:49:56 +0530

    Create app.py
    Api for model

---
+++ ml/app.py
@@ -0,0 +1,24 @@
+# Using flask to make an api 
+# import necessary libraries and functions 
+from flask import Flask, jsonify, request 
+from flask_restful import Resource, Api 
+# creating a Flask app 
+app = Flask(__name__) 
+api=Api(app)
+  
+
+class Hello(Resource):
+	def get(self):
+		return jsonify({'data':'hello world'})
+	def post(self):
+		data=request.get_json()
+		return jsonify(data)
+
+api.add_resource(Hello,'/')
+
+
+
+# driver function 
+if __name__ == '__main__': 
+  
+    app.run(debug = True) 
