d91d9eb4b46fd2e439a8dcf655b9a01d6100f9ec
Pranjalya <pranjalyawarrior@gmail.com>
2020-08-05 20:42:42 +0530

    api files fixed

--- ml/toxic_generator_api/test.py
+++ ml/toxic_generator_api/test.py
@@ -1,8 +1,9 @@
 import requests
 
 if __name__ == "__main__":
-    url = 'http://127.0.0.1:5000/gettoxic'
+    url = 'http://15.207.99.158:6000/gettoxic'
 
     response =  requests.post(url)
+    import pdb; pdb.set_trace()
     result = response.json()
-    pritn(result)
+    print(result)
